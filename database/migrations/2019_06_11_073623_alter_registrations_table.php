962c5c68c9712c98ef920753f9f7974cbac34450
duongsieu <duongdosieu@gmail.com>
2019-06-21 13:46:31 +0700

    fix

--- database/migrations/2019_06_11_073623_alter_registrations_table.php
+++ database/migrations/2019_06_11_073623_alter_registrations_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AlterRegistrationsTable extends Migration
 {
@@ -16,6 +16,15 @@ class AlterRegistrationsTable extends Migration
         Schema::table('registrations', function (Blueprint $table) {
             $table->string('at_time')->comment = 'Morning; Afternoon; Full';
             $table->integer('status')->default(0)->nullable()->change()->comment = '0 - empty; 1- approved; 2 - disapproved; 3 - pending';
+            $table->unsignedDecimal('annual_leave_total', 3, 1)->nullable()->change();
+            $table->unsignedDecimal('annual_leave', 3, 1)->nullable()->change();
+            $table->unsignedDecimal('annual_leave_unused', 3, 1)->nullable()->change();
+            $table->unsignedDecimal('sick_leave', 3, 1)->nullable()->change();
+            $table->unsignedDecimal('marriage_leave', 3, 1)->nullable()->change();
+            $table->unsignedDecimal('maternity_leave', 3, 1)->nullable()->change();
+            $table->unsignedDecimal('bereavement_leave', 3, 1)->nullable()->change();
+            $table->unsignedDecimal('long_term_unpaid_leave', 3, 1)->nullable()->change();
+            $table->unsignedDecimal('short_term_unpaid_leave', 3, 1)->nullable()->change();
         });
     }
 
